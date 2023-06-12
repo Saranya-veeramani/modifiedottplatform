53ff983bdad8744f2ab6ef93cf72848723abb1ee
Saranya-veeramani <120944072+Saranya-veeramani@users.noreply.github.com>
2023-06-12 17:56:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Movies from './pages/Movies';
+import Footer from './components/Footer';
+import TvSeries from './pages/TvSeries';
+import Documentaries from './pages/Documentaries';
+import MovieDetails from './components/MovieDetailsPage'; // Import the MovieDetails component
+import './App.css';
+
+const App = () => {
+  return (
+<div className='main'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes><Route path='/' element={<Movies />} />
+                            <Route path='/movies' element={<Movies />} />
+          <Route path='/movie-details/:imdbid' element={<MovieDetails />} /> {/* New route for MovieDetails */}
+        </Routes>    
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
